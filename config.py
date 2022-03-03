89e47619f7fe7203c0d552f6b8e8fa78aa1bcfe0
Harshitha-bacha <workingforeverlifelong@gmail.com>
2022-03-03 10:53:16 +0530

    first commit

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost="harshitha-db.c21k1fprfjub.ap-south-1.rds.amazonaws.com"
+customuser="admin"
+custompass="Honey.11850"
+customdb="employee"
+custombucket="harshithabucket1"
+customregion="ap-south-1"
+
